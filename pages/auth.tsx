e97011ccb658bdeb7bfff34044070f4e818688af
Mukhtar Rafi Fauzi <mukhtar.r.f@gmail.com>
2023-04-16 14:33:04 +0700

    setup prisma and mongodb, add register feature

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,5 +1,6 @@
-import Input from "@/components/input"
+import axios from "axios";
 import { useCallback, useState } from "react"
+import Input from "@/components/input"
 
 const Auth = () => {
     const [email, setEmail] = useState('');
@@ -10,7 +11,19 @@ const Auth = () => {
 
     const toggleVariant = useCallback(() => {
         setVariant((currentVariant) => currentVariant == 'login' ? 'register' : 'login');
-    }, [])
+    }, []);
+
+    const register = useCallback(async () => {
+        try {
+            await axios.post('api/register', {
+                email,
+                name,
+                password,
+            });
+        } catch (error) {
+            console.log(error)
+        }
+    }, [email, name, password]);
 
     return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -47,7 +60,7 @@ const Auth = () => {
                             value={password}
                         />
                     </div>
-                    <button className="bg-red-500 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                    <button onClick={register} className="bg-red-500 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
                         {variant == 'login' ? 'Login' : 'Sign up'}
                     </button>
                     <p className="text-neutral-500 mt-12">
