45db06fac83fc36903a6e0f3f38d2bdab6343d7f
Mukhtar Rafi Fauzi <mukhtar.r.f@gmail.com>
2023-04-18 16:01:38 +0700

    add movie list and movie card component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
-import { NextPageContext } from "next"
-import { getSession } from "next-auth/react"
+import React from 'react';
+import { NextPageContext } from 'next';
+import { getSession } from 'next-auth/react';
 
-import Navbar from "@/components/Navbar";
-import Billboard from "@/components/Billboard";
+import Navbar from '@/components/Navbar';
+import Billboard from '@/components/Billboard';
+import MovieList from '@/components/MovieList';
+// import InfoModal from '@/components/InfoModal';
+import useMovieList from '@/hooks/useMovieList';
+// import useFavorites from '@/hooks/useFavorites';
+// import useInfoModalStore from '@/hooks/useInfoModalStore';
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -17,15 +23,24 @@ export async function getServerSideProps(context: NextPageContext) {
   }
 
   return {
-    props: {},
+    props: {}
   }
 }
 
 export default function Home() {
+  const { data: movies = [] } = useMovieList();
+  // const { data: favorites = [] } = useFavorites();
+  // const {isOpen, closeModal} = useInfoModalStore();
+
   return (
     <>
+      {/* <InfoModal visible={isOpen} onClose={closeModal} /> */}
       <Navbar />
       <Billboard />
+      <div className="pb-40">
+        <MovieList title="Trending Now" data={movies} />
+        {/* <MovieList title="My List" data={favorites} /> */}
+      </div>
     </>
   )
 }
